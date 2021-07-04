fc313c3e02fa31e5e944f6d6e08f9ca9be755a7c
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-07-05 10:45:12 +1200

    Duplicate licensesList from list.go
    This is in reference to the review comments in [#30](https://github.com/nishanths/license/pull/30)
    
    https://github.com/nishanths/license/pull/30#discussion_r663487229

--- license.go
+++ license.go
@@ -7,8 +7,35 @@ import (
 	"text/template"
 )
 
+var knownLicenses = map[string]struct {
+	longName string
+	template string
+}{
+	"agpl-3.0":     {"GNU Affero General Public License v3.0", Agpl30Template},
+	"apache-2.0":   {"Apache License 2.0", Apache20Template},
+	"bsd-2-clause": {"BSD 2-Clause \"Simplified\" License", Bsd2ClauseTemplate},
+	"bsd-3-clause": {"BSD 3-Clause \"New\" or \"Revised\" License", Bsd3ClauseTemplate},
+	"cc0-1.0":      {"Creative Commons Zero v1.0 Universal", Cc010Template},
+	"epl-2.0":      {"Eclipse Public License 2.0", Epl20Template},
+	"free-art-1.3": {"Free Art License 1.3", FreeArt13Template},
+	"gpl-2.0":      {"GNU General Public License v2.0", Gpl20Template},
+	"gpl-3.0":      {"GNU General Public License v3.0", Gpl30Template},
+	"lgpl-2.1":     {"GNU Lesser General Public License v2.1", Lgpl21Template},
+	"lgpl-3.0":     {"GNU Lesser General Public License v3.0", Lgpl30Template},
+	"mit":          {"MIT License", MitTemplate},
+	"mpl-2.0":      {"Mozilla Public License 2.0", Mpl20Template},
+	"unlicense":    {"The Unlicense", UnlicenseTemplate},
+	"wtfpl":        {"Do What The Fuck You Want To Public License", WtfplTemplate},
+}
+
+func printList() {
+	for key, license := range knownLicenses {
+		stdout.Printf("%-14s(%s)", key, license.longName)
+	}
+}
+
 func printLicense(license, output, name, year string) {
-	licenseData, ok := licensesList[license]
+	licenseData, ok := knownLicenses[license]
 	if !ok {
 		stderr.Printf("unknown license %q\nrun \"license -list\" for list of available licenses", license)
 		os.Exit(2)
