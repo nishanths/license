edc6c16bf590017caf88e72065868a17940e4228
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-07-05 13:32:09 +1200

    Fix tabs v spaces indentation

--- main.go
+++ main.go
@@ -20,17 +20,17 @@ const (
 	usageString = `Usage: license [flags] [license-type]
 
 Flags:
-       -help     print help information
-       -list     print list of available license types
-   -n, -name     full name to use on license (default %q)
-   -o, -output   path to output file (prints to stdout if unspecified)
-       -version  print version
-   -y, -year     year to use on license (default %q)
+	-help     print help information
+	-list     print list of available license types
+	-n, -name     full name to use on license (default %q)
+	-o, -output   path to output file (prints to stdout if unspecified)
+	    -version  print version
+	-y, -year     year to use on license (default %q)
 
 Examples:
-  license mit
-  license -name "Alice L" -year 2013 bsd-3-clause
-  license -o LICENSE.txt mpl-2.0`
+	license mit
+	license -name "Alice L" -year 2013 bsd-3-clause
+	license -o LICENSE.txt mpl-2.0`
 )
 
 var (
